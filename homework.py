7f7528a64d9fbaf1469a30363c43d89ab7974b21
Artakharutyunyan120 <artak.ah120@gmail.com>
2021-09-23 20:05:06 +0400

    homwork1

---
+++ homework.py
@@ -0,0 +1,25 @@
+
+#import random
+# x=random.randint(1,20)
+# while x>10:
+# 	print('true')
+# 	if x<=10:
+# 		break
+# x=0
+# for x in range(0,50,5):
+#	x+=1
+# 	print(x)
+
+
+
+# for i in range(1,10):
+# 	if i == 5:
+# 		continue
+# 	print(i)
+
+import random
+random.randint('qar','tuxt','mkrat')	
+random.randint('qar','tuxt','mkrat')
+for x in pc:
+	for y in mart:
+		print(x,y)

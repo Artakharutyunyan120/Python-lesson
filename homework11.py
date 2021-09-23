95db0932392578e1e15f3d45c14b40740e9feace
Artakharutyunyan120 <artak.ah120@gmail.com>
2021-09-23 20:06:50 +0400

    homework11

---
+++ homework11.py
@@ -0,0 +1,22 @@
+# for i in range(0,100):
+# 	if i%12==0 and i%15==0:
+# 		print(i)
+
+
+# for i in range(1,100):
+# 	if i%2==0:
+# 		print(i)
+
+# for i in range(1,100):
+# 	if i%2!=0:
+# 		print(i)
+
+# word='helo worf'
+# print(f'{word:^35}')
+# print(f'{word:-^35}')
+# print(f'{word:#^35}')
+'''genre'''
+
+
+x,y=234,'*'
+print(x*y)

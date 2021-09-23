8af49da38e3128e0353c42beec2669036913f769
Artakharutyunyan120 <artak.ah120@gmail.com>
2021-09-23 20:16:17 +0400

    homework19

---
+++ homework19.py
@@ -0,0 +1,41 @@
+# x={1,2,4,'gndak'}
+# y={3,5,7,'sexan'}
+# z=x.union(y)
+# print(z)
+
+set1 = {12,23,'11',"hello"}
+set2 = {14,3,'141',"world"}
+# print(set1.isdisjoint(set2))
+# for i in set1:
+# 	print(i)
+# set1.add(15)
+# print(set1)
+
+# set1.update('16','44',barlus)
+# print(set1)
+
+# set1 = {14,3,12,23,'11',"hello"}
+# set2 = {14,3,'141',"world"}
+# set3=set1.intersection(set2)
+# print(set3)
+
+# set1 = {12,23,'11',"hello"}
+# set2 = {14,3,'141',"world"}
+# set1.remove(12)
+# print(set1)
+
+set1 = {12,23,'11',"hello"}
+if 12 in set1:
+	print("ayo ka")
+	set1.remove(12)
+print(set1)
+
+# set1 = {12,23,'11',"hello"}
+# print(len(set1))
+
+# set1 = {12,23,56,78,88}
+# print(max(set1))
+
+
+# set1 = {12,23,56,78,88}
+# print(min(set1))

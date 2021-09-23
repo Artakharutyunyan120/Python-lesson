67cd70608a16a23d73a93836b4614c84b8db9a4d
Artakharutyunyan120 <artak.ah120@gmail.com>
2021-09-23 20:06:06 +0400

    homework9

---
+++ homework9.py
@@ -0,0 +1,46 @@
+# x1=int(input('mutq areq 1 tiv@ '))
+# x2=int(input('mutq areq 2 tiv@ '))
+# if x1>x2:
+# 	print('mece arji tiv@')
+# elif x2>x1:
+# 	print('mece erkrord tiv@')
+# else:
+# 	print('tver@ havasar en')
+
+mard1=int(input('mutqagreq tariq@ '))
+mard2=int(input('mutqagreq tariq@ '))
+mard3=int(input('mutqagreq tariq@ '))
+if mard1> mard2>mard3:
+	print('archi mard@ amena tarecne')
+	if mard1>mard2>mard3:
+		print("amena eritasart@ erord mardne")
+elif mard2>mard1>mard3:
+	print('erkrort mardne amena tarec@')
+	if mard2>mard1>mard3:
+		print("amena eritasart@ erord mardne")
+elif mard3>mard2>mard1:
+	print('erord mardne amena tarec@ ')
+	if mard3>mard2>mard1:
+		print("amena eritasart@ araji mardne")
+else:
+	print('NRANQ MI TARIQIEN')
+
+
+
+# f=str(input('mutqagreq tiv@ '))#skzbic str-ov tiv@ sarqumenq string vor karananq frcnenq
+# res=f[::-1]
+# print(res)
+
+
+# ser=str(input('mutqagreq dzer ser@ ' ))
+# tariq=int(input('mutqagreq dzer tariq@ '))
+# kragavichak=input('amusnacac eq te voch Y/N ')
+# if ser=='man' and tariq >20 and tariq <40:
+# 	print('na karoxe ashxatel amenur')
+# 	if ser=='kin'and tariq>20 and tariq<40:
+# 		print('na kashxati miayn qaxaqayin paymannerum')
+# elif ser=='man' and tariq>40:
+# 	print('40 ic mec txamartiq kashxaten qaxaqum')
+# else:
+# 	print('duq cheq hamapatasxanum mer tvyalnerin')
+

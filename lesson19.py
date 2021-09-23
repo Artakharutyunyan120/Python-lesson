3ac639782174b192f0b8ce710ac08d4850ea7dc0
Artakharutyunyan120 <artak.ah120@gmail.com>
2021-09-23 19:43:19 +0400

    my first commit

---
+++ lesson19.py
@@ -0,0 +1,96 @@
+# for i,j in zip([1,2],[31,12]):
+# 	print(i,j)
+
+
+# c= [1,2,3,4,1,2,3]
+# print(set(c))
+
+
+# c={'ani','aram',12,True}
+# c.add('1234')
+# print(c,type(c))
+
+# c.update({123,123,1})
+# print(c)
+
+
+# c={'ani','aram',12,True}
+# #c.remuve('Angelina')
+# c.discard('Angelina')
+# print(c)
+
+
+# set1={12,23,'11','helo'}
+# set2={14,3,141,'world'}
+# print(set1.isdisjoint(set2))
+
+
+
+# x=[12,34,'raf',45]
+# x.append(124)
+# print(x)
+
+# y=['Artush','Armen','Artur']
+# y.append('spo')
+# print(y)
+
+
+# y=['Artush','Armen','Artur']
+# # y.clear()
+# # print(y)
+
+# x=[12,34,'raf',45]
+# x.clear()
+# print(x)
+
+# x.copy()
+# print(x)
+
+# y.copy()
+# print(y)
+
+# y=['Artush','Armen','Artur']
+# for i in y:
+# 	print(i,y.count(i))
+
+# x=[12,32,54,67,67,32]
+# for i in x:
+# 	print(i,x.count(i))
+
+
+# y=['Artush','Armen','Artur']
+# y.extend('Arman')
+# print(y)
+
+# z=['ashxar']
+# z.extend(z)
+# print(z)
+
+
+# y=['Artush','Armen','Artur']
+# print(y.index('Artush'))
+
+# y=['Artush','Armen','Artur']
+# print(y.index('Artur'))
+
+# y=['Artush','Armen','Artur']
+# # y.pop()
+# # print(y)
+# y.remove('Armen')
+# print(y)
+
+# y=['Artush','Armen','Artur']
+# y.reverse()
+# print(y)
+# y=[1,2,34,4,56]
+# y.reverse()
+# print(y)
+
+
+# x=[1,3,6,4,9,10,]
+# x.sort()
+# print(x)
+
+x=[1,5,6,9,2,4,3,7,10,22]
+x.sort()
+print(x)
